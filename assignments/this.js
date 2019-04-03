6a841eb8c2744f01c9e61003daa9b94d89c63d86
anthony <stackchain@gmail.com>
2019-04-03 12:42:39 -0400

    small change in definitions

--- assignments/this.js
+++ assignments/this.js
@@ -2,9 +2,9 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. Window Binding - when you envoke a function that uses the `this` keyword without using any of the other 3 methods, the `this` keyword defaults to the window object.
-* 2. Implicit Binding - is used the most. When a function is envoked, look to the left of the dot and that is what the `this` keyworkd references.
+* 2. Implicit Binding - (objects generally) is used the most. When a function is envoked, look to the left of the dot and that is what the `this` keyworkd references.
 * 3. New binding - used when you create a new object with the `new` keyword, the `this` keyword is bound to the new object.
-* 4. Explicit Binding - you use it when you call a function with an argument using .call, .apply or .bind and the argument is the `this` statement reference.
+* 4. Explicit Binding - (functions generally) you use it when you call a function with an argument using .call, .apply or .bind and the argument is the `this` statement reference.
 *
 * write out a code example of each explanation above
 */
